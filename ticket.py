33fd7ba6c57176a23d4664ee21f255ffce894868
Jiechi-wq <Jiechic@nycstudents.net>
2024-12-11 10:16:15 -0500

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -1,12 +1,23 @@
 import random
 import os
+
 class ticket():
-    def lotto(self):
-        n1 = input("Choose a number from 1-70: ")
-        n2 = input("Choose a number from 1-70: ")
-        n3 = input("Choose a number from 1-70: ")
-        n4 = input("Choose a number from 1-70: ")
-        n5 = input("Choose a number from 1-70: ")
-        return (n1, n2, n3, n4, n5)
-Lotto = ticket()
-Lotto.lotto()
+    def lotto():
+        number = [input("Choose a number from 1-70: ") for _ in range(5)]
+        return tuple(number)
+    def rlotto():
+        numbers = list(range(1,71))    
+        random_number = [random.choice(numbers) for _ in range(5)]
+        print(random_number)
+    def reveal(number, random_number):
+        for i in number:
+            if i == random_number:
+                print("cool")
+            elif i != random_number:
+                print("lose")
+    lotto()
+
+
+
+
+
