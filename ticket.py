fb573c4efe7e1137187ab014d79156ed8783263d
Jiechi-wq <Jiechic@nycstudents.net>
2024-12-16 10:16:02 -0500

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -1,24 +1,21 @@
 import random
-import os
 
 class ticket():
     def lotto():
         guess = [input("Choose a number from 1-70: ") for _ in range(5)] #Chatgpt made it loop 5 times instead of 5 lines of the same code
-        return tuple(guess)
+        return guess
     def rlotto():
         numbers = list(range(1,71))    
         random_number = [random.choice(numbers) for _ in range(5)]
         print(random_number)
         return random_number
     def reveal():
-        for i in guessing:
-            if i == real:
-                print("cool")
-            elif i != real:
-                print("lose")
+        if guess == real:
+            print("win")
+        elif guess != real:
+            print("lose")
 real = ticket.rlotto()
-guessing = ticket.lotto()
-
+guess = ticket.lotto()
 show = ticket.reveal()
 
 
