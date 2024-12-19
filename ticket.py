c7efb020f854bb53bec612a10f7201445b5bb1e3
Jiechi-wq <Jiechic@nycstudents.net>
2024-12-19 10:04:44 -0500

    whigte

--- ticket.py
+++ ticket.py
@@ -1,16 +1,18 @@
 import random
-
-
+numbers = list(range(1,71))
 class ticket():
     def __init__(self):
         self.guess = []
         self.random_number = []
     def lotto(self):
-        self.guess = [int(input("Choose a number from 1-70: ")) for _ in range(5)] #Chatgpt made it loop 5 times instead of 5 lines of the same code
-        if 1 <= self.guess <= 70:
-            return self.guess
-        else:
-            print("Enter a valid number")
+        while True:
+            try:
+                self.guess = [int(input("Choose a number from 1-70: ")) for _ in range(5)] #Chatgpt made it loop 5 times instead of 5 lines of the same code      
+                if self.guess not in numbers:
+                    print("Please choose a number between 1 and 70: ")
+            except ValueError:
+                print("Invalid value, enter an integer: ")
+                return self.guess
     def rlotto(self):
         self.random_number = [random.choice(range(1, 71)) for _ in range(5)]
         return self.random_number
@@ -25,5 +27,3 @@ tickets.lotto()
 tickets.reveal()
 
 
-"""         except ValueError:
-            print("Invalid input. Please enter a valid integer.") """
